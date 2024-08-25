2f573d84ca9892f75e9485e2b71cc1f25af9a78e
vikas <rawatvikas689@gmail.com>
2024-08-26 00:14:03 +0530

    first commit

---
+++ src/Cyl.jsx
@@ -0,0 +1,28 @@
+import { useTexture } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
+import * as THREE from "three";
+
+function Cyl() {
+  let tex = useTexture("images/images.png");
+  tex.needsUpdate = true;
+  let cyl = useRef(null);
+  useFrame((state, delta) => {
+    cyl.current.rotation.y += delta;
+  });
+  return (
+    <>
+      <group  rotation={[0, 1.4, 0.5]}>
+        {/* x,y,z  */}
+        <mesh ref={cyl}>
+          {/* <boxGeometry args={[1, 1, 1]} /> */}
+          {/* <cylinderGeometry args={[radiusTop : Float, radiusBottom : Float, height : Float, radialSegments : Integer, heightSegments : Integer, openEnded : Boolean,]} /> */}
+          <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+          <meshStandardMaterial map={tex} side={THREE.DoubleSide} />
+        </mesh>
+      </group>
+    </>
+  );
+}
+
+export default Cyl;
