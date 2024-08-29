2d3643ac692f8ae197159ec34046c8054f064a4d
vikas <rawatvikas689@gmail.com>
2024-08-30 00:37:57 +0530

    commit

---
+++ src/NewCreation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PlaneGeometry } from 'three'
+
+function NewCreation() {
+  return (
+    <>
+      <mesh>
+        <PlaneGeometry/>
+        <meshBasicMaterial/>
+      </mesh>
+    </>
+  )
+}
+
+export default NewCreation
