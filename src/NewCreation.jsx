e4a0595fa22874e7b5df7c37082a766d234298ec
vikas <rawatvikas689@gmail.com>
2024-08-30 01:54:37 +0530

    commit

--- src/NewCreation.jsx
+++ src/NewCreation.jsx
@@ -10,15 +10,15 @@ function NewCreation() {
           <boxGeometry args={[1, 0.1, 1]} />
           <meshStandardMaterial color="hotpink" />
         </mesh>
-        {/* <Text
-          position={[0, 0, 0]}
+        <Text
+          position={[0, 1, 0]}
           fontSize={0.3}
           color="hotpink"
           anchorX="center"
           anchorY="middle"
         >
           Nano Chip!
-        </Text> */}
+        </Text>
         <mesh position={[0, -0.3, 0]}>
           <boxGeometry args={[1, 0.1, 1]} />
           <meshStandardMaterial color="yellow" />
