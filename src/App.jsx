edc1a33b6ddfe0c928813618b421a5c88b13f413
vikas <rawatvikas689@gmail.com>
2024-09-02 20:08:40 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {
 } from "@react-three/postprocessing";
 import NewCreation from "./NewCreation";
 function App() {
-  return (   
+  return (  
     <>          
       <Canvas flat camera={{ fov: 90 }}>
         <OrbitControls />
