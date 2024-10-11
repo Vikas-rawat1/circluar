1425161dceb5db280d97af46d5eb565d26ce8888
vikas <rawatvikas689@gmail.com>
2024-10-11 19:25:42 +0530

    commit

--- src/NewCreation.jsx
+++ src/NewCreation.jsx
@@ -8,7 +8,7 @@ import { Text } from "@react-three/drei";
       <group>
         <mesh position={[0, -0.1, 0]}>
           <boxGeometry args={[1, 0.1, 1]} />
-          <meshStandardMaterial color="hotpink" />
+          {/* <meshStandardMaterial color="hotpink" /> */}
         {/* </mesh> */}
         <Text
           position={[0, 1, 0]}
