686307729159ebee8531af192bd12546cf66e5d3
vikas <rawatvikas689@gmail.com>
2024-10-14 19:10:55 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Canvas } from "@react-three/fiber";
 // import React from "react";
 // import "./style.css";
 // import { OrbitControls } from "@react-three/drei";
-import Cyl from "./Cyl";
 import {
   Bloom,
   EffectComposer,
