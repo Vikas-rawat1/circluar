feb07fe8cdbcad79e4733eb012752cb11b9c4410
vikas <rawatvikas689@gmail.com>
2024-10-14 19:12:03 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <EffectComposer>
           <Bloom
             mipmapBlur
-            intensity={17.0}
+            // intensity={17.0}
             luminanceThreshold={0}
             luminanceSmoothing={0}
           />
